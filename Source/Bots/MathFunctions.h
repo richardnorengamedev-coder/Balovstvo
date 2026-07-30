0f3e0169bfe90f3ecdda0f442ec8891a302ef765
richardnorengamedev-coder <richardnorengamedev@gmail.com>
2026-07-31 02:33:22 +0300

    Add Spiral settings struct and SpiralPoint function

--- Source/Bots/MathFunctions.h
+++ Source/Bots/MathFunctions.h
@@ -6,6 +6,38 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MathFunctions.generated.h"
 
+USTRUCT(BlueprintType)
+struct FSpiralSettings
+{
+    GENERATED_BODY()
+
+    // Radius at Alpha = 0
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral")
+    float StartRadius = 100.0f;
+
+    // Radius at Alpha = 1
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral")
+    float EndRadius = 100.0f;
+
+    // Number of full turns over Alpha = 0..1
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral")
+    float Turns = 1.0f;
+
+    // Total translation along the axis over Alpha = 0..1
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral")
+    float Height = 0.0f;
+
+    // Start angle in degrees
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral")
+    float StartAngleDeg = 0.0f;
+
+    // If true the spiral winds clockwise when looking along the axis direction
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spiral")
+    bool bClockwise = false;
+
+    FSpiralSettings() {}
+};
+
 /**
  * 
  */
@@ -18,4 +50,14 @@ public:
 
     UFUNCTION(BlueprintPure, Category = "Math|Easing")
     static float EaseOutExpo(float Alpha);
+
+    /**
+     * Получает точку на спирали.
+     * - Origin: центр/начало спирали
+     * - Axis: ось спирали (направление "вверх" для подъёма)
+     * - Settings: параметры спирали (радиусы, витки, высота и т.п.)
+     * - Alpha: 0..1 (положение вдоль спирали)
+     */
+    UFUNCTION(BlueprintPure, Category = "Math|Spiral", meta=(DisplayName = "Spiral Point"))
+    static FVector SpiralPoint(const FVector& Origin, const FVector& Axis, const FSpiralSettings& Settings, float Alpha);
 };
